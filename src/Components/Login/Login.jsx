03060568057407229e03e680491db89ae6b7b360
EA03 <00175420@uca.edu.sv>
2021-11-16 12:03:58 -0600

    Update: login

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,18 +1,16 @@
 import user_logo from '../../Img/Logo_user.png';
 const Login = () => {
     return (
-        <div className="items-center min-h-screen bg-blue-400 flex justify-center" 
-        /* class="w-full h-full"
-        style="background-image: url('../../Img/banner.jpg')"*/> 
+        <div className="items-center min-h-screen  bg-blue-400 flex justify-center" >
             <main className="">
                 <form className="flex flex-col items-center bg-gray-100 py-8 rounded px-10  ">
                     <h1 className="mb-5 text-3xl text-center">Login</h1>
                     <img src={user_logo} alt="user" className="w-20" />
                     <label className="font-bold">Username</label>
-                    <input type="text" placeholder="Enter username or email..." className="my-2 flex-grow h-8 px-2 rounded border border-grey-400 text-center"/>
+                    <input type="text" placeholder="Enter username or email..." className="my-2 flex-grow h-8 px-8 rounded border border-grey-400 text-center"/>
                     <label className="font-bold">Password</label>
-                    <input type="password" placeholder="Enter password..." className="my-2 flex-grow h-8 px-2 rounded border border-grey-400 text-center" />
-                    <button type="submit" className="rounded bg-blue-500 hover:bg-blue-700 text-white text-sm font-semibold py-2 px-8">Login</button>
+                    <input type="password" placeholder="Enter password..." className="my-2 flex-grow h-8 px-8 rounded border border-grey-400 text-center" />
+                    <button type="submit" className="rounded bg-blue-500 hover:bg-blue-700 text-white text-sm font-semibold py-2 px-8 mt-2">Login</button>
                 </form>
             </main>
         </div>
